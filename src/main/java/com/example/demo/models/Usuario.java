f4148b34357ac695b6c45c8bb8dc93e5f89aebff
ASantiagoNunezG <145824482+ASantiagoNunezG@users.noreply.github.com>
2024-05-27 01:34:53 -0500

    Revert "Lupa"

---
+++ src/main/java/com/example/demo/models/Usuario.java
@@ -0,0 +1,11 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    int id_usuario;
+    String ga;
+}
